819bbf6776fb0bf8ae91b9b2538f967ac6cf1a63
Brett Levine <brett@concreteinteractive.com>
2014-09-07 20:34:22 -0700

    - Added rich text editor - Display html in list and form views

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 gem 'activeadmin', github: 'activeadmin'
+gem 'active_admin_editor'
 gem 'devise'
 
 # Use ActiveModel has_secure_password
--- app/admin/hex.rb
+++ app/admin/hex.rb
@@ -3,9 +3,25 @@ ActiveAdmin.register Hex do
   permit_params :hex_text, :hex_category_id
 
   index do
-    column :hex_text
+    column (:hex_text) { |html_allowed| raw(html_allowed.hex_text) }
     column :hex_category
     actions
+  end
+
+  show do |ad|
+    attributes_table do
+      row :hex_category
+      row (:hex_text) { |html_allowed| raw(html_allowed.hex_text) }
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :hex_category
+      f.input :hex_text, as: :html_editor
+    end
+    f.actions
   end
 
 end
